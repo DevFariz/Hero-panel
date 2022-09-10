231027787c5be30bf5fbc9489e9c3844c77982e4
R. Fariz <farizramazanov38@gmail.com>
2022-09-10 18:25:54 +0400

    feat: add functionality

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,25 +1,69 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {v4 as uuidv4} from "uuid";
+import { heroCreated } from "../../actions";
+import {useHttp} from "../../hooks/http.hook";
+import Spinner from "../spinner/Spinner";
 
+const HeroesAddForm = () => {   
+    const [heroName, setHeroName] = useState('');
+    const [heroDescr, setHeroDescr] = useState('');
+    const [heroElement, setHeroElement] = useState('');
 
-// Задача для этого компонента:
-// Реализовать создание нового героя с введенными данными. Он должен попадать
-// в общее состояние и отображаться в списке + фильтроваться
-// Уникальный идентификатор персонажа можно сгенерировать через uiid
-// Усложненная задача:
-// Персонаж создается и в файле json при помощи метода POST
-// Дополнительно:
-// Элементы <option></option> желательно сформировать на базе
-// данных из фильтров
+    const {request} = useHttp();
 
-const HeroesAddForm = () => {
+    const {filters, filtersLoadingStatus} = useSelector(state => state);
+    const dispatch = useDispatch();
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        const newHero = {
+            id: uuidv4 (),
+            name: heroName,
+            description: heroDescr,
+            element: heroElement
+        }
+
+        request("http://localhost:3001/heroes", "POST", JSON.stringify(newHero))
+            .then(res => console.log(res, 'Отправка успешна'))
+            .then(dispatch(heroCreated(newHero)))
+            .catch(err => console.log(err));
+        
+        setHeroName('');
+        setHeroDescr('');
+        setHeroElement('');
+    }
+
+    const renderFilters = (filters, status) => {
+        if(status === 'loading'){
+            return <option><Spinner /></option>
+        } else if(status === 'error'){
+            return <option>Ошибка загрузки</option>
+        }
+
+        if (filters && filters.length > 0 ) {
+            return filters.map(({name, label}) => {
+                // Один из фильтров нам тут не нужен
+                // eslint-disable-next-line
+                if (name === 'all')  return;
+
+                return <option key={name} value={name}>{label}</option>
+            })
+        }
+    }
+
     return (
-        <form className="border p-4 shadow-lg rounded">
+        <form className="border p-4 shadow-lg rounded" onSubmit={onSubmitHandler}>
             <div className="mb-3">
                 <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
                 <input 
                     required
                     type="text" 
                     name="name" 
-                    className="form-control" 
+                    className="form-control"
+                    onChange={(e) => setHeroName(e.target.value)}
+                    value={heroName}
                     id="name" 
                     placeholder="Как меня зовут?"/>
             </div>
@@ -29,7 +73,9 @@ const HeroesAddForm = () => {
                 <textarea
                     required
                     name="text" 
-                    className="form-control" 
+                    className="form-control"
+                    onChange={(e) => setHeroDescr(e.target.value)}
+                    value={heroDescr} 
                     id="text" 
                     placeholder="Что я умею?"
                     style={{"height": '130px'}}/>
@@ -41,12 +87,10 @@ const HeroesAddForm = () => {
                     required
                     className="form-select" 
                     id="element" 
+                    onChange={(e) => setHeroElement(e.target.value)}
                     name="element">
-                    <option >Я владею элементом...</option>
-                    <option value="fire">Огонь</option>
-                    <option value="water">Вода</option>
-                    <option value="wind">Ветер</option>
-                    <option value="earth">Земля</option>
+                    <option>Я владею элементом...</option>
+                    {renderFilters(filters, filtersLoadingStatus)}
                 </select>
             </div>
 
