f52d6a2c2e25f293b35b1eeb69ec4a950857ec35
R. Fariz <farizramazanov38@gmail.com>
2022-09-13 17:48:11 +0400

    fix: modify store creation

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -26,7 +26,6 @@ const HeroesAddForm = () => {
         }
 
         request("http://localhost:3001/heroes", "POST", JSON.stringify(newHero))
-            .then(res => console.log(res, 'Отправка успешна'))
             .then(dispatch(heroCreated(newHero)))
             .catch(err => console.log(err));
         
